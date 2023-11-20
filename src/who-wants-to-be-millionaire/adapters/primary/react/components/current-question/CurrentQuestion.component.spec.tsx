a9f18116f17b09e2e0981a6a88833e5fbf3a3249
Michaël AZERHAD <mazerhad@wealcomecompany.com>
2023-11-20 20:52:38 +0100

    add a second component test to ensure pyramid step highlight upon a good answer

--- src/who-wants-to-be-millionaire/adapters/primary/react/components/current-question/CurrentQuestion.component.spec.tsx
+++ src/who-wants-to-be-millionaire/adapters/primary/react/components/current-question/CurrentQuestion.component.spec.tsx
@@ -4,29 +4,52 @@ import {UseCasesContext} from "../../useCasesInjections.tsx";
 import {QuestionGatewayStub} from "../../../../secondary/gateways/questionGatewayStub.ts";
 import {validateAnswer} from "../../../../../core-logic/use-cases/answer-validation/validateAnswer.ts";
 import App from "../App.tsx";
+import "@testing-library/jest-dom";
 
 describe('Question component', () => {
 
-    it('should inform about a correct given answer',
-        async () => {
-            const questionGateway = new QuestionGatewayStub();
-            questionGateway.currentValidation = {B: true}
-            // given
-            let informedAboutCorrectAnswer: boolean = false;
-            vitest.spyOn(window, 'alert')
-                .mockImplementation((message: string) => {
-                    if (message)
-                        informedAboutCorrectAnswer = true;
-                });
-            render(<UseCasesContext.Provider value={{
-                validateAnswer: validateAnswer(questionGateway)
-            }}>
-                <App/>
-            </UseCasesContext.Provider>);
-            // when
-            await userEvent.click(screen.getByText('Test-Driven Development'));
-            // then
-            expect(informedAboutCorrectAnswer).toBeTruthy();
-        });
+    let questionGateway: QuestionGatewayStub;
 
+    beforeEach(() => {
+        questionGateway = new QuestionGatewayStub();
+        questionGateway.currentValidation = {B: true}
+    });
+
+    describe('Upon a first correct answer', () => {
+
+        it('should inform about the correctness',
+            async () => {
+                // given
+                let informedAboutCorrectAnswer: boolean = false;
+                vitest.spyOn(window, 'alert')
+                    .mockImplementation((message: string) => {
+                        if (message)
+                            informedAboutCorrectAnswer = true;
+                    });
+                renderAppComponent();
+                // when
+                await userEvent.click(screen.getByText('Test-Driven Development'));
+                // then
+                expect(informedAboutCorrectAnswer).toBeTruthy();
+            });
+
+        it('should increase the pyramid step',
+            async () => {
+                // given
+                renderAppComponent();
+                // when
+                await userEvent.click(screen.getByText('Test-Driven Development'));
+                // then
+                expect(screen.getByText('0 €')).not.toHaveClass('rounded-full bg-orange-500');
+                expect(screen.getByText('1000000 €')).toHaveClass('rounded-full bg-orange-500');
+            });
+    });
+
+    const renderAppComponent = () => {
+        render(<UseCasesContext.Provider value={{
+            validateAnswer: validateAnswer(questionGateway)
+        }}>
+            <App/>
+        </UseCasesContext.Provider>);
+    }
 });
