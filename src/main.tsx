072510a6ae66ffe5b0a7dc55c62f828b913f221b
Michaël AZERHAD <mazerhad@wealcomecompany.com>
2023-11-20 12:55:43 +0100

    validate answer on click on the given answer

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {validateAnswer} from "./who-wants-to-be-millionaire/core-logic/use-cases/answer-validation/validateAnswer.ts";
+import {QuestionGatewayStub} from "./who-wants-to-be-millionaire/core-logic/gateways/questionGatewayStub.ts";
+
+const questionGateway = new QuestionGatewayStub();
+questionGateway.currentValidation = {A: false, B: true, C: false, D: false};
+
+export type UseCasesDef = {
+    validateAnswer: ReturnType<typeof validateAnswer>
+}
+
+const defaultUseCasesContextValue: UseCasesDef = {
+    validateAnswer: validateAnswer(questionGateway)
+};
+
+export const UseCasesContext = React.createContext<UseCasesDef>(defaultUseCasesContextValue)
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <UseCasesContext.Provider value={defaultUseCasesContextValue}>
+        <App />
+    </UseCasesContext.Provider>
   </React.StrictMode>,
 )
--- src/who-wants-to-be-millionaire/PossibleAnswers.component.tsx
+++ src/who-wants-to-be-millionaire/PossibleAnswers.component.tsx
@@ -1,16 +1,27 @@
+import {useContext} from "react";
+import {UseCasesContext} from "../main.tsx";
+
 export const PossibleAnswers = () => {
+
+    const {validateAnswer} = useContext(UseCasesContext);
+
+  const validateThatAnswer = (givenThatAnswer: string) => async () => {
+      const validation = await validateAnswer(givenThatAnswer);
+      alert(validation);
+  }
+
   return (
     <div className="w-full justify-center grid grid-cols-2 text-white gap-4 font-mono text-sm text-left font-bold leading-6 bg-stripes-fuchsia rounded-lg">
-      <div className="border-3 border-blue-300 rounded-lg px-3 py-1 bg-gray-900">
+      <div className="border-3 border-blue-300 rounded-lg px-3 py-1 bg-gray-900" onClick={(validateThatAnswer('A'))}>
         <span className="text-orange-500">A:</span> Third-Driven Development
       </div>
-      <div className="border-3 border-blue-300 rounded-lg px-3 py-1 bg-gray-900">
+      <div className="border-3 border-blue-300 rounded-lg px-3 py-1 bg-gray-900" onClick={(validateThatAnswer('B'))}>
         <span className="text-orange-500">B:</span> Test-Driven Development
       </div>
-      <div className="border-3 border-blue-300 rounded-lg px-3 py-1 bg-gray-900">
+      <div className="border-3 border-blue-300 rounded-lg px-3 py-1 bg-gray-900" onClick={(validateThatAnswer('C'))}>
         <span className="text-orange-500">C:</span> Test-Deep Development
       </div>
-      <div className="border-3 border-blue-300 rounded-lg px-3 py-1 bg-gray-900">
+      <div className="border-3 border-blue-300 rounded-lg px-3 py-1 bg-gray-900" onClick={(validateThatAnswer('D'))}>
         <span className="text-orange-500">D:</span> Test-Driven Design
       </div>
     </div>
